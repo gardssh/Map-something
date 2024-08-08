c5bee98b677fc6231e347d57dc88c6b9df4a7013
Stian Forseth <stian-fo@hotmail.com>
2024-08-08 16:46:03 +0200

    remove old code

--- src/components/MapComponent/index.tsx
+++ src/components/MapComponent/index.tsx
@@ -1,14 +1,9 @@
 'use client'
-import Head from 'next/head'
 import Map from "react-map-gl";
 
 export const MapComponent = () => {
   return (
     <div className="h-full w-full">
-    <Head>
-      <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v3.5.2/mapbox-gl.css' rel='stylesheet' />
-
-    </Head>
       <Map
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
     mapLib={import('mapbox-gl')}
