4f3e3ece6c61c8ef5b57689d4924572da27d9997
Stian Forseth <stian-fo@hotmail.com>
2024-08-13 17:02:47 +0200

    stop fetching data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,19 @@ import { MapComponent } from '@/components/MapComponent/index';
 import { useSession } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import SideBar from '@/components/SideBar';
+import activities from '../../public/aktiviteter.json';
 
 const TOKEN_ENDPOINT = 'https://www.strava.com/oauth/token';
 
 export default function Home() {
-	const [activities, setActivities] = useState<any[]>([]);
+	//const [activities, setActivities] = useState<any[]>([]);
 	const { data: session, status } = useSession();
 
+	/* comment out to stop polling API too many times
 	useEffect(() => {
 		const getActivities = async () => {
 			let moreData = true;
-			const maxCount = 5;
+			const maxCount = 2;
 			let page = 1;
 			const per_page = 5;
 			const tempActivities = [];
@@ -47,6 +49,7 @@ export default function Home() {
 			getActivities();
 		}
 	}, [activities, session, status]);
+     */
 	console.log(activities);
 
 	return (
