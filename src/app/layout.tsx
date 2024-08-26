d55674b45d1d78c4c0203bef355bfa9b218307a2
Stian Forseth <stian-fo@hotmail.com>
2024-08-26 16:42:43 +0200

    remove unused import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css';
 import Provider from '@/app/context/client-provider';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import SideBar from '@/components/SideBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
