7dec841e7f2c8e795c8002541731c1925f9c3360
gardssh <garddun@gmail.com>
2024-08-27 23:37:45 +0200

    Played around with sidebar.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,10 +4,30 @@ import { useRef, useEffect, useCallback } from 'react';
 import { Badge } from './ui/badge';
 import { Button } from './ui/button';
 import { Route, MapPin, Medal } from 'lucide-react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from './ui/card';
 import './SideBar/styles.css';
-
+import { formatTime } from '@/lib/timeFormat';
 import { signIn, signOut } from 'next-auth/react';
+
+import { TrendingUp } from 'lucide-react';
+import { CartesianGrid, Line, LineChart, XAxis } from 'recharts';
+
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+
+const chartData = [
+	{ month: 'January', desktop: 186 },
+	{ month: 'February', desktop: 305 },
+	{ month: 'March', desktop: 237 },
+	{ month: 'April', desktop: 73 },
+	{ month: 'May', desktop: 209 },
+	{ month: 'June', desktop: 214 },
+];
+const chartConfig = {
+	desktop: {
+		label: 'Desktop',
+		color: 'hsl(var(--chart-1))',
+	},
+} satisfies ChartConfig;
 
 export default function SideBar({
 	activities,
@@ -36,8 +56,68 @@ export default function SideBar({
 	return (
 		<div className="w-1/3 flex flex-col gap-4 relative">
 			{selectedActivity ? (
-				<div id="slide" className="bg-slate-100 grow p-4 flex flex-col gap-4 relative">
+				<div id="slide" className=" grow p-4 flex flex-col gap-4 relative">
 					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{selectedActivity.name}</h3>
+					<div className="flex justify-between gap-2">
+						<Badge variant={'secondary'} className="flex-1 flex items-center justify-center">
+							{selectedActivity.type}
+						</Badge>
+						<Badge variant={'secondary'} className="flex-1 flex items-center justify-center">
+							{selectedActivity.start_date.slice(0, 10)}
+						</Badge>
+					</div>
+					<Card>
+						<CardHeader>
+							<p>Distance: {(selectedActivity.distance / 1000).toFixed(2)} km</p>
+						</CardHeader>
+					</Card>
+
+					<Card>
+						<CardHeader>
+							<p>Moving time: {formatTime(selectedActivity.moving_time)}</p>
+						</CardHeader>
+					</Card>
+
+					<Card>
+						<CardHeader>
+							<p>Total elevation gain: {selectedActivity.total_elevation_gain} m</p>
+						</CardHeader>
+					</Card>
+
+					<Card>
+						<CardHeader>
+							<p>Moving speed: {(selectedActivity.average_speed * 3.6).toFixed(2)} km/t</p>
+						</CardHeader>
+					</Card>
+
+					<Card>
+						<CardHeader>
+							<CardTitle>Elevation</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<ChartContainer config={chartConfig}>
+								<LineChart
+									accessibilityLayer
+									data={chartData}
+									margin={{
+										left: 12,
+										right: 12,
+									}}
+								>
+									<CartesianGrid vertical={false} />
+									<XAxis
+										dataKey="month"
+										tickLine={false}
+										axisLine={false}
+										tickMargin={8}
+										tickFormatter={(value) => value.slice(0, 3)}
+									/>
+									<ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+									<Line dataKey="desktop" type="natural" stroke="var(--color-desktop)" strokeWidth={2} dot={false} />
+								</LineChart>
+							</ChartContainer>
+						</CardContent>
+					</Card>
 				</div>
 			) : (
 				<div className="p-4 flex flex-col gap-4 relative grow">
@@ -71,7 +151,7 @@ export default function SideBar({
 					<div className="grow gap-2 overflow-auto">
 						<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight mb-4">Nearby</h3>
 						{/* Disse kortene skal ta inn ting om aktiviteter */}
-						{visibleActivities.map((activity) => (
+						{visibleActivities.map((activity: any) => (
 							<div key={activity.id}>
 								<Card className={'mb-2' + (activity.id === selectedRouteId && ' border-solid border-2 border-black')}>
 									<CardHeader>
