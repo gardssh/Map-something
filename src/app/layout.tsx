6b172e096cb7dce607de8b229133b905abcfc881
gardssh <garddun@gmail.com>
2024-12-13 13:33:49 +0100

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
 	title: 'Villspor',
 	description: 'Et kart for meg som er på villspor',
+	icons: {
+		icon: '/favicon.svg',
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
